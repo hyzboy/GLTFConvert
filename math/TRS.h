9ed6967188fda9fdb0767847cd7da786cbd8ca68
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-10-06 11:34:49 +0800

    TRS移出pure名字空间

--- math/TRS.h
+++ math/TRS.h
@@ -3,15 +3,10 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 
-namespace pure
+// Restore TRS-based transform representation (float for real-time use)
+struct TRS
 {
-
-    // Restore TRS-based transform representation (float for real-time use)
-    struct TRS
-    {
-        glm::vec3 translation{ 0.0f };
-        glm::quat rotation{ 1.0f,0.0f,0.0f,0.0f }; // w,x,y,z
-        glm::vec3 scale{ 1.0f };
-    };
-
-} // namespace pure
+    glm::vec3 translation{ 0.0f };
+    glm::quat rotation{ 1.0f,0.0f,0.0f,0.0f }; // w,x,y,z
+    glm::vec3 scale{ 1.0f };
+};
