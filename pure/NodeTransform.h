ff5a290bc3416b8fcf51c078550c73ab1daa30d9
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-10-06 13:08:52 +0800

    修正NodeTransform

--- pure/NodeTransform.h
+++ pure/NodeTransform.h
@@ -18,17 +18,17 @@ namespace pure
 
         const NodeTransform &operator = (const std::variant<fastgltf::TRS, fastgltf::math::fmat4x4> &n)
         {
-            if(std::holds_alternative<fastgltf::TRS>(n.transform))
+            if(std::holds_alternative<fastgltf::TRS>(n))
             {
                 is_matrix=false;
 
-                trs=std::get<fastgltf::TRS>(n.transform);
+                trs=std::get<fastgltf::TRS>(n);
             }
             else
             {
                 is_matrix=true;
                 
-                const auto &mat=std::get<fastgltf::math::fmat4x4>(n.transform);
+                const auto &mat=std::get<fastgltf::math::fmat4x4>(n);
 
                 m=toMat4(mat);
             }
